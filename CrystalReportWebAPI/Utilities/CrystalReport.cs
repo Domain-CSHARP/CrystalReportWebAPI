8247a2c9a758e4d49869e8ba4072d2a600f39895
leeyongyi <leeyongyi21@gmail.com>
2025-12-05 08:25:46 +0800

    Use odbc

--- CrystalReportWebAPI/Utilities/CrystalReport.cs
+++ CrystalReportWebAPI/Utilities/CrystalReport.cs
@@ -29,15 +29,13 @@ namespace CrystalReportWebAPI.Utilities
             rd.Load(Path.Combine(System.Web.Hosting.HostingEnvironment.MapPath(reportPath), reportFileName));
 
             // Set database connection
-            string connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
-            var builder = new SqlConnectionStringBuilder(connectionString);
-            string server = builder.DataSource;
-            string database = builder.InitialCatalog;
-            bool integratedSecurity = builder.IntegratedSecurity;
+            GetConnectionInfo("DefaultConnection", out string server, out string database, out string userId, out string password, out bool integratedSecurity, out string providerName, out string fullConnectionString);
 
             // Debug: Log current Windows identity for troubleshooting
             string currentUser = System.Security.Principal.WindowsIdentity.GetCurrent().Name;
             System.Diagnostics.Debug.WriteLine($"CrystalReport: Current Windows identity: {currentUser}");
+            System.Diagnostics.Debug.WriteLine($"CrystalReport: Provider: {providerName}");
+            System.Diagnostics.Debug.WriteLine($"CrystalReport: Full Connection String: {fullConnectionString}");
             System.Diagnostics.Debug.WriteLine($"CrystalReport: Using Integrated Security: {integratedSecurity}");
             System.Diagnostics.Debug.WriteLine($"CrystalReport: Server: {server}, Database: {database}");
 
@@ -45,7 +43,14 @@ namespace CrystalReportWebAPI.Utilities
 foreach (Table table in rd.Database.Tables)
 {
     var logonInfo = table.LogOnInfo;
-    logonInfo.ConnectionInfo.ServerName = server;
+    if (string.Equals(providerName, "System.Data.Odbc", StringComparison.OrdinalIgnoreCase))
+    {
+        logonInfo.ConnectionInfo.ServerName = fullConnectionString;
+    }
+    else
+    {
+        logonInfo.ConnectionInfo.ServerName = server;
+    }
     logonInfo.ConnectionInfo.DatabaseName = database;
     if (integratedSecurity)
     {
@@ -53,8 +58,8 @@ foreach (Table table in rd.Database.Tables)
     }
     else
     {
-        logonInfo.ConnectionInfo.UserID = builder.UserID;
-        logonInfo.ConnectionInfo.Password = builder.Password ?? string.Empty;
+        logonInfo.ConnectionInfo.UserID = userId;
+        logonInfo.ConnectionInfo.Password = password ?? string.Empty;
         logonInfo.ConnectionInfo.IntegratedSecurity = false;
     }
     table.ApplyLogOnInfo(logonInfo);
@@ -69,7 +74,14 @@ foreach (ReportDocument subreport in rd.Subreports)
     foreach (Table table in subreport.Database.Tables)
     {
         var logonInfo = table.LogOnInfo;
-        logonInfo.ConnectionInfo.ServerName = server;
+        if (string.Equals(providerName, "System.Data.Odbc", StringComparison.OrdinalIgnoreCase))
+        {
+            logonInfo.ConnectionInfo.ServerName = fullConnectionString;
+        }
+        else
+        {
+            logonInfo.ConnectionInfo.ServerName = server;
+        }
         logonInfo.ConnectionInfo.DatabaseName = database;
         if (integratedSecurity)
         {
@@ -77,8 +89,8 @@ foreach (ReportDocument subreport in rd.Subreports)
         }
         else
         {
-            logonInfo.ConnectionInfo.UserID = builder.UserID;
-            logonInfo.ConnectionInfo.Password = builder.Password ?? string.Empty;
+            logonInfo.ConnectionInfo.UserID = userId;
+            logonInfo.ConnectionInfo.Password = password ?? string.Empty;
             logonInfo.ConnectionInfo.IntegratedSecurity = false;
         }
         table.ApplyLogOnInfo(logonInfo);
@@ -155,17 +167,20 @@ foreach (ReportDocument subreport in rd.Subreports)
             rd.Load(Path.Combine(System.Web.Hosting.HostingEnvironment.MapPath(reportPath), reportFileName));
 
             // Set database connection
-            string connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
-            var builder = new SqlConnectionStringBuilder(connectionString);
-            string server = builder.DataSource;
-            string database = builder.InitialCatalog;
-            bool integratedSecurity = builder.IntegratedSecurity;
+            GetConnectionInfo("DefaultConnection", out string server, out string database, out string userId, out string password, out bool integratedSecurity, out string providerName, out string fullConnectionString);
 
             // Main report tables
             foreach (Table table in rd.Database.Tables)
             {
                 var logonInfo = table.LogOnInfo;
-                logonInfo.ConnectionInfo.ServerName = server;
+                if (string.Equals(providerName, "System.Data.Odbc", StringComparison.OrdinalIgnoreCase))
+                {
+                    logonInfo.ConnectionInfo.ServerName = fullConnectionString;
+                }
+                else
+                {
+                    logonInfo.ConnectionInfo.ServerName = server;
+                }
                 logonInfo.ConnectionInfo.DatabaseName = database;
                 if (integratedSecurity)
                 {
@@ -173,8 +188,8 @@ foreach (ReportDocument subreport in rd.Subreports)
                 }
                 else
                 {
-                    logonInfo.ConnectionInfo.UserID = builder.UserID;
-                    logonInfo.ConnectionInfo.Password = builder.Password ?? string.Empty;
+                    logonInfo.ConnectionInfo.UserID = userId;
+                    logonInfo.ConnectionInfo.Password = password ?? string.Empty;
                     logonInfo.ConnectionInfo.IntegratedSecurity = false;
                 }
                 table.ApplyLogOnInfo(logonInfo);
@@ -189,7 +204,14 @@ foreach (ReportDocument subreport in rd.Subreports)
                 foreach (Table table in subreport.Database.Tables)
                 {
                     var logonInfo = table.LogOnInfo;
-                    logonInfo.ConnectionInfo.ServerName = server;
+                    if (string.Equals(providerName, "System.Data.Odbc", StringComparison.OrdinalIgnoreCase))
+                    {
+                        logonInfo.ConnectionInfo.ServerName = fullConnectionString;
+                    }
+                    else
+                    {
+                        logonInfo.ConnectionInfo.ServerName = server;
+                    }
                     logonInfo.ConnectionInfo.DatabaseName = database;
                     if (integratedSecurity)
                     {
@@ -197,8 +219,8 @@ foreach (ReportDocument subreport in rd.Subreports)
                     }
                     else
                     {
-                        logonInfo.ConnectionInfo.UserID = builder.UserID;
-                        logonInfo.ConnectionInfo.Password = builder.Password ?? string.Empty;
+                        logonInfo.ConnectionInfo.UserID = userId;
+                        logonInfo.ConnectionInfo.Password = password ?? string.Empty;
                         logonInfo.ConnectionInfo.IntegratedSecurity = false;
                     }
                     table.ApplyLogOnInfo(logonInfo);
@@ -254,6 +276,59 @@ foreach (ReportDocument subreport in rd.Subreports)
             rd.ExportToDisk(ExportFormatType.PortableDocFormat, outputFilePath);
             rd.Close();
             rd.Dispose();
+        }
+        private static void GetConnectionInfo(string connectionStringName, out string server, out string database, out string userId, out string password, out bool integratedSecurity, out string providerName, out string fullConnectionString)
+        {
+            var settings = ConfigurationManager.ConnectionStrings[connectionStringName];
+            fullConnectionString = settings.ConnectionString;
+            providerName = settings.ProviderName;
+
+            if (string.Equals(providerName, "System.Data.Odbc", StringComparison.OrdinalIgnoreCase))
+            {
+                var builder = new System.Data.Odbc.OdbcConnectionStringBuilder(fullConnectionString);
+                
+                // Extract Server
+                if (builder.ContainsKey("Server"))
+                    server = builder["Server"] as string;
+                else if (builder.ContainsKey("Data Source"))
+                    server = builder["Data Source"] as string;
+                else
+                    server = string.Empty;
+
+                // Extract Database
+                if (builder.ContainsKey("Database"))
+                    database = builder["Database"] as string;
+                else if (builder.ContainsKey("Initial Catalog"))
+                    database = builder["Initial Catalog"] as string;
+                else
+                    database = string.Empty;
+
+                // Extract Integrated Security
+                integratedSecurity = false;
+                if (builder.ContainsKey("Trusted_Connection"))
+                {
+                    string trusted = builder["Trusted_Connection"] as string;
+                    integratedSecurity = "yes".Equals(trusted, StringComparison.OrdinalIgnoreCase) || "true".Equals(trusted, StringComparison.OrdinalIgnoreCase);
+                }
+
+                // Extract User/Password
+                userId = string.Empty;
+                if (builder.ContainsKey("User Id")) userId = builder["User Id"] as string;
+                else if (builder.ContainsKey("Uid")) userId = builder["Uid"] as string;
+
+                password = string.Empty;
+                if (builder.ContainsKey("Password")) password = builder["Password"] as string;
+                else if (builder.ContainsKey("Pwd")) password = builder["Pwd"] as string;
+            }
+            else
+            {
+                var builder = new SqlConnectionStringBuilder(fullConnectionString);
+                server = builder.DataSource;
+                database = builder.InitialCatalog;
+                integratedSecurity = builder.IntegratedSecurity;
+                userId = builder.UserID;
+                password = builder.Password;
+            }
         }
     }
 }
